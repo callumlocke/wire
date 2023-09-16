29e8ff1b7cb40f23b0bc69b536467020866c7d9c
Callum Locke <callum.locke@gmail.com>
2023-09-16 11:12:24 +0100

    use createMatcher in branch fn

--- core/branch.ts
+++ core/branch.ts
@@ -1,17 +1,17 @@
 /**
- * Creates a transform that runs multiple transforms in parallel on different subsets of the input and then merges the results.
+ * Creates a transform that runs multiple transforms in parallel on different subsets of the input and then merges the results. Takes a `transforms` object, the keys of which are globs, and the values of which are transforms to apply to whichever files match those globs.
  *
- * Subsets are exclusive. If a file matches the first key, it won't be included in the second key's subset.
+ * Branches are exclusive. If a file matches the first key, it won't be included in the second key's subset.
  *
  * Merging works like this: if a file is present in multiple outputs, the last one wins.
  *
  * NB. key order is standardised as of ES2015: https://stackoverflow.com/a/5525820
  * (respects the order, except for number-like keys)
  *
- * This is like subset, except that it only takes globs, not any Matchable.
+ * This is very similar to subset, except that it only takes glob strings, not any Matchable, so it can offer a more readable syntax for cases where you want to do a few things in parallel.
  */
 
-import globToRegExp from 'glob-to-regexp'
+import { createMatcher } from '..'
 import { Filemap, Transform } from '../types'
 
 export const branch =
@@ -24,9 +24,8 @@ export const branch =
     const jobs: Record<string, Promise<Filemap>> = {}
 
     for (const glob of Object.keys(transforms)) {
-      const matchedFiles = inputFiles.filter((name) =>
-        globToRegExp(glob).test(name)
-      )
+      const match = createMatcher(glob)
+      const matchedFiles = inputFiles.filter((name) => match(name))
 
       // exclude matched files from the next glob
       inputFiles = inputFiles.filter((name) => !matchedFiles.includes(name))
